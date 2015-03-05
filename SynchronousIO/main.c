32b4c38060ae393596e8947b9e8c2fa79f014b18
plasmashadow <plasmashadowx@gmail.com>
2015-03-05 07:44:27 +0530

    adding synchronous io excersice

--- SynchronousIO/main.c
+++ SynchronousIO/main.c
@@ -7,16 +7,19 @@
 #include <string.h>
 #include <error.h>
 
+#define _XOPEN_SOURCE
 extern int errno;
 
 int main(void)
 {
     int fd=creat("newtext.txt",0777);
-    char* msg="hey hello how are you\n";
+    char* msg="hey hello how are you this is some crap in this file so please ignore it\n";
     off_t ret;
+    ssize_t bits;
+    char* buf;
     close(fd);
     /*O_SYNC flag enables us to open a file in synchronous way i.e sync the read writes files with the buffer.*/
-    fd=open("newtext.txt",O_WRONLY|O_SYNC);
+    fd=open("newtext.txt",O_RDWR|O_SYNC);
     if(fd==-1)
         perror("error while opening the file");
     /*seeking is just moving around the file*/
@@ -27,6 +30,12 @@ int main(void)
     if(ret==-1)
         perror("error while seeking");
     printf("%d has moved\n",ret);
+
+    /*POSTITIONAL READS AND WRITES*/
+    bits=pread(fd,buf,20,2);
+    if(bits==-1)
+        perror("error while reading a the give position");
+    printf(buf);
     return EXIT_SUCCESS;
 }
 
